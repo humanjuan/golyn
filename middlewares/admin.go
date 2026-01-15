b8288ad4530f70a87b26caa8e0fdc460265077a1
jpengineer <jalejandro.ingeniero@gmail.com>
2026-01-15 20:28:56 -0300

    feat: add `SuperAdminMiddleware` and improve role verification in `AdminMiddleware`
    - Introduced `SuperAdminMiddleware` for stricter access control requiring "SuperAdmin" role.
    - Enhanced `AdminMiddleware` to support case-insensitive comparison of roles for "Admin" and "SuperAdmin".

--- middlewares/admin.go
+++ middlewares/admin.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/humanjuan/golyn/globals"
@@ -12,11 +13,30 @@ import (
 func AdminMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		log := globals.GetAppLogger()
-		role, exists := c.Get("role")
+		roleVal, exists := c.Get("role")
+		role := fmt.Sprintf("%v", roleVal)
 
-		if !exists || (role != "SuperAdmin" && role != "Admin") {
+		if !exists || (strings.ToLower(role) != "superadmin" && strings.ToLower(role) != "admin") {
 			log.Warn("AdminMiddleware() | Access denied | User: %s | Role: %v", c.GetString("subject"), role)
 			err := fmt.Errorf("insufficient privileges")
+			c.Error(utils.NewHTTPError(http.StatusForbidden, err.Error()))
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func SuperAdminMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log := globals.GetAppLogger()
+		roleVal, exists := c.Get("role")
+		role := fmt.Sprintf("%v", roleVal)
+
+		if !exists || strings.ToLower(role) != "superadmin" {
+			log.Warn("SuperAdminMiddleware() | Access denied | User: %s | Role: %v", c.GetString("subject"), role)
+			err := fmt.Errorf("insufficient privileges: SuperAdmin only")
 			c.Error(utils.NewHTTPError(http.StatusForbidden, err.Error()))
 			c.Abort()
 			return
